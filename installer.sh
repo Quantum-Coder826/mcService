f15db261fa2bd38210dba82c28ec44228b74e746
Quantum-Coder826 <b.veldthuis@gmail.com>
2023-07-11 12:17:18 +0200

    added daemon reload

--- installer.sh
+++ installer.sh
@@ -8,10 +8,11 @@ ENDCOLOR="\e[0m"
 
 mkdir -p ~/.config/systemd/user/
 echo -e "${BLUE}Downloading service file${ENDCOLOR}"
-
 wget -O ~/.config/systemd/user/mc@.service https://raw.githubusercontent.com/Quantum-Coder826/mcService/master/mc%40.service
-echo -e "${BLUE}Creating server folder${ENDCOLOR}"
 
+echo -e "${BLUE}Creating server folder${ENDCOLOR}"
 mkdir -p ~/minecraft/
+
+systemctl --user daemon-reload
 echo -e "${GREEN}Instalation is done.\nSee: https://github.com/Quantum-Coder826/mcService for the docs. ${ENDCOLOR}"
 exit 0
